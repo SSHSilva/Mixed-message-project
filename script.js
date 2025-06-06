80b87334c124852059b6c308b84b40570fedd4d1
Sharon <170877911+SSHSilva@users.noreply.github.com>
2025-06-06 11:15:49 +0100

    Created an array and a function to select random message and added the date object

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const dailyMessage = ['Your present circumstances don\'t determine where you can go; they merely determine where you start.', 'The best way to predict your future is to create it.', 'It does not matter how slowly you go, as long as you do not stop.', 'Be not afraid of growing slowly; be afraid only of standing still.', 'When it is obvious that the goals cannot be reached, don\'t adjust the goals, adjust the action steps.', 'A journey of a thousand miles begins with a single step.'];
+
+
+function randomMessage(array) {
+ const message = array[Math.floor(Math.random() * array.length)];
+ const dayOfTheWeek = new Date().toLocaleDateString('en-GB', {weekday: 'long'});
+ return `Today is ${dayOfTheWeek}: "${message}"`;
+
+}
+
+console.log(randomMessage(dailyMessage));
